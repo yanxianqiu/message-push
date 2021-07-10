dda4f2e383e8e8bd5fff6dc7e7fb0b3294329b40
yanxianqiu <1791211331@qq.com>
2021-07-10 16:57:03 +0800

    启动配置项

---
+++ websocket-message/src/main/java/com/yan/message/StartSpringbootProject.java
@@ -0,0 +1,11 @@
+package com.yan.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringbootProject {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringbootProject.class,args);
+    }
+}
