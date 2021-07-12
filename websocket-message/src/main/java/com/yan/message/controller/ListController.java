699e0a86ccc23c7c4bbf217a98306f76fbf15687
yanxianqiu <1791211331@qq.com>
2021-07-12 23:03:38 +0800

    去除跳转方法

--- websocket-message/src/main/java/com/yan/message/controller/ListController.java
+++ websocket-message/src/main/java/com/yan/message/controller/ListController.java
@@ -12,14 +12,7 @@ public class ListController {
 
     @Autowired
     private WebSocketMessage webSocket;
-    /**
-     * 接收推送来的消息的页面
-     * @return
-     */
-    @RequestMapping(value ="/pushNotice")
-    public String list(){
-        return "pushNotice";
-    }
+
     /**
      * 触发推送
      * 举例：list方法可以是订单列表，这个方法可以是下单的接口，下单后，list的订单列表提示收到新订单。
